f4868e102074e7354bdc82eebdafdcc3d5706448
Matt <matthewwjohnson@nevada.unr.edu>
2022-05-04 12:06:58 -0700

    Removed profiling for ScriptHubExport
    - Removed nearly all profiling for ScriptHubExport
    - Removed use of mono_image_findClass since it is now slower than mono_image_findClassSlow
    - Renamed mono_image_findClassSlow to mono_image_findClass_for_ScriptHub
    - Removed unneeded spaces from json ouput.

--- CheatEngineMod/autorun/ScriptHubExport.lua
+++ CheatEngineMod/autorun/ScriptHubExport.lua
@@ -1,12 +1,6 @@
-
-elapsed1 = 0.0
-elapsed2 = 0.0
-elapsed3 = 0.0
-elapsed4 = 0.0
-elapsed5 = 0.0
-
 classTable = nil
 
+-- Creates a table of assemblies which each have a table of all of their classes in the global variable classTable
 function PreComputeClasses()
   if (monopipe==nil)  then
     LaunchMonoDataCollector()
@@ -20,10 +14,6 @@ function PreComputeClasses()
     classTable[assemblyName] = {}
     classTable[assemblyName]["image"] = assembly
     classTable[assemblyName]["classes"] = {}
-    if assemblyName == "System.Xml" then
-      print("System.Xml found!")
-      print(tostring(classTable[assemblyName]))
-    end
     local classes = mono_image_enumClasses(assembly)
     for classIndex=1, #classes do
       local name = classes[classIndex].classname
@@ -56,36 +46,12 @@ end
 
 function ScriptHubExport(fileLoc)
   local clockA = os.clock()
-  local clockB 
-  local clockC 
-  local clockD
-  local clockE
-  local clockF
-  local clockG
-  local clockH
-  local clockZ
-  local clockW
-  local clockX
-  local clockY
-  local elapsedB = 0.0
-  local elapsedC = 0.0
-  local elapsedD = 0.0
-  local elapsedE = 0.0
-  local elapsedF = 0.0
-  local elapsedG = 0.0
-  local elapsedH = 0.0
-  local elapsedZ = 0.0
-  if classTable==nil then
-    PreComputeClasses()
-  end
+  PreComputeClasses()
   if (monopipe==nil)  then
     LaunchMonoDataCollector()
   end
-  clockY = os.clock()
   local image=getImage()
-  clockX = os.clock()
   local classes=mono_image_enumClasses(image)
-  clockW = os.clock()
   local i,j
   local outputString = "{\"classes\" : {"
   if classes~=nil then
@@ -95,9 +61,7 @@ function ScriptHubExport(fileLoc)
     for i=1, stopValue do --#classes do
       value = i
       -- Ensure class has a name for comparisons
-      clockB = os.clock()
       classes[i].fqname = mono_class_getFullName(classes[i].class)
-      clockZ = os.clock()
       --classes[i].name = classes[i].classname-- mono_class_getName(node.Data)
       if classes[i].fqname==nil or classes[i].fqname=='' then
         classes[i].fqname=classes[i].classname
@@ -108,11 +72,8 @@ function ScriptHubExport(fileLoc)
       --print(classes[i].fqname)
       -- Only continue if the class has a valid name note: short circuit eval works in LUA
       if classes[i].fqname~=nil and classes[i].fqname~='' and classes[i].fqname~='<unnamed>' and not string.find(classes[i].fqname, "<") then
-        clockC = os.clock()
         local classData = mono_findClass_ScriptHub(classes[i].namespace, classes[i].classname) -- retrieve the class object reference (not a string)
-        clockD = os.clock()
         local fields = mono_class_enumFields_ScriptHub(classData, false) -- 2nd parameter is whether to include offsets from parent classes
-        clockE = os.clock()
         local parent = mono_class_getParent(classes[i].class)
         -- Build JSON string from fields
         if fields ~= nil and #fields > 0 then
@@ -120,49 +81,23 @@ function ScriptHubExport(fileLoc)
           local tempClass = classes[i]
           outputString = outputString.."\""..tempClass.fqname.."\" : {\"ShortName\": \""..tempClass.classname.."\","
           if parent ~= nil then
-              local parentName = mono_class_getFullName(parent)
-              outputString = outputString.."\"Parent\": \""..parentName.."\","
+            local parentName = mono_class_getFullName(parent)
+            outputString = outputString.."\"Parent\": \""..parentName.."\","
           end
-          clockF = os.clock()
           outputString = outputString.." \"fields\" : {"
           for j=1, #tempClass.fields do
-              outputString = outputString.."\""..tempClass.fields[j].name.."\" : {".. "\"offset\" : \""..tempClass.fields[j].offset.."\", \"type\" :\""..tempClass.fields[j].typename.."\", \"static\" :\""..tostring(tempClass.fields[j].isStatic).."\"}"
-              if j < #tempClass.fields then
-                outputString = outputString..","
-              else
-                outputString = outputString.."}"
-              end
+            outputString = outputString.."\""..tempClass.fields[j].name.."\":{".."\"offset\":\""..tempClass.fields[j].offset.."\",\"type\":\""..tempClass.fields[j].typename.."\",\"static\":\""..tostring(tempClass.fields[j].isStatic).."\"}"
+            if j < #tempClass.fields then
+              outputString = outputString..","
+            else
+              outputString = outputString.."}"
+            end
           end
-          clockG = os.clock()
           outputString = outputString.."}"
           if i < stopValue then
-              outputString = outputString..","
+            outputString = outputString..","
           end
-
-
-          if(clockF ~= nil and clockE ~= nil) then
-            elapsedE = clockF-clockE + elapsedE
-          end
-          if(clockG ~= nil and clockF ~= nil) then
-            elapsedF = clockG-clockF + elapsedF
         end
-        
-        if(clockD ~= nil and clockC ~= nil) then
-          elapsedC = clockD-clockC + elapsedC
-        end
-        if(clockE ~= nil and clockD ~= nil) then
-          elapsedD = clockE-clockD + elapsedD
-        end
-
-      end
-      
-      if(clockC ~= nil and clockB ~= nil) then
-        elapsedB = clockC-clockB + elapsedB
-      end
-
-      end
-      if(clockB ~= nil and clockZ ~= nil) then
-        elapsedF = clockZ-clockB + elapsedF
       end
     end
     -- test in case the last classes read was nil/empty/unamed or <> and didn't add a new object as expected
@@ -185,21 +120,6 @@ function ScriptHubExport(fileLoc)
     clockH = os.clock()
     
     print("Total Elapsed time: "..tostring(os.clock()-clockA))
-    print("B Elapsed time: "..tostring(elapsedB))
-    print("C Elapsed time: "..tostring(elapsedC))
-    print("D Elapsed time: "..tostring(elapsedD))
-    print("E Elapsed time: "..tostring(elapsedE))
-    print("F Elapsed time: "..tostring(elapsedF))
-    print("G Elapsed time: "..tostring(elapsedG))
-    
-    print("Y Elapsed time: "..tostring(clockY-clockA))
-    print("X Elapsed time: "..tostring(clockX-clockY))
-    print("W Elapsed time: "..tostring(clockW-clockX))
-    
-    print("mono_enumAssemblies Elapsed time: "..tostring(elapsed1))
-    print("mono_image_findClass Elapsed time: "..tostring(elapsed2))
-    print("mono_image_findClassSlow Elapsed time: "..tostring(elapsed3))
-
   else
     print("getClass failed to find classes")
   end
@@ -267,47 +187,20 @@ end
 
 --searches all images for a specific class
 function mono_findClass_ScriptHub(namespace, classname)
-  local clockA = os.clock()
-
-  local clockB 
-  local clockC 
-  local clockD
-
   local ass=mono_enumAssemblies()
-
-  clockB = os.clock()
-  elapsed1 = clockB - clockA + elapsed1
-
   local result
     if ass==nil then return nil end
   for i=1, #ass do
-    result=mono_image_findClass(mono_getImageFromAssembly(ass[i]), namespace, classname)
+    result=mono_image_findClass_for_ScriptHub(mono_getImageFromAssembly(ass[i]), namespace, classname)
     if (result~=0) then
-      clockC = os.clock()
-      elapsed2 = clockC - clockB + elapsed2
-      return result;
-    end
-  end
-
-  clockC = os.clock()
-  elapsed2 = clockC - clockB + elapsed2
-  -- print("mono_image_findClass Elapsed time: "..tostring(elapsed2))
-
-  --still here:
-  for i=1, #ass do
-    result=mono_image_findClassSlow_ScriptHub(mono_getImageFromAssembly(ass[i]), namespace, classname)
-    if (result~=0) then
-      clockD = os.clock()
-      elapsed3 = clockD - clockC + elapsed3
       return result;
     end
   end  
-  -- no clock, results here were 0 with normal functioning reads so never reaches here.
   return nil
 end
 
 -- find a class in a specific image
-function mono_image_findClassSlow_ScriptHub(image, namespace, classname)
+function mono_image_findClass_for_ScriptHub(image, namespace, classname)
   local result=0
   if monopipe==nil then return 0 end 
   local assemblyName = mono_image_get_name(image)
